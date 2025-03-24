a95f15def2e7e62945e4a3edf4b6d669ebc15a91
luisaferRP <luisaramiresporras103@gmail.com>
2025-03-23 20:51:58 -0500

    feat: se crea vista y funcionalidad para eliminar tarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,53 @@
 import '../scss/style.scss'
 import javascriptLogo from '../javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from '../counter.js'
+import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener("DOMContentLoaded",()=>{
+  const inputTarea = document.getElementById("nueva-tarea");
+  const botonAgregar = document.getElementById("agregar-tarea");
+  const listaTareas = document.getElementById("lista-tareas");
 
-setupCounter(document.querySelector('#counter'))
+  //agregar tarea
+  function agregarTarea() {
+    const textoTarea = inputTarea.value.trim()
+    if (textoTarea === "") {
+      alert("Escriba una tarea,ehh cansonaa.");
+      return;
+    }
+
+    //crear tarea con clave texto y valor
+    const tarea = {texto: textoTarea}
+    pintarTareaDOM(tarea);
+
+    inputTarea.value = "";
+
+  }
+
+  //pintar tarea
+  function pintarTareaDOM(tarea) {
+    const elementli = document.createElement("li");
+    elementli.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+    elementli.textContent = tarea.texto;
+
+    //boton eliminar
+    const botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "❌";
+    botonEliminar.classList.add("btn","btn-sm");
+    botonEliminar.addEventListener("click",()=>{
+      elementli.remove();
+    });
+    //añadimos el boton al elemento
+    elementli.appendChild(botonEliminar);
+    //pintamos en el ul el li
+    listaTareas.appendChild(elementli);
+
+  }
+
+  //boton para agregar tareas
+  botonAgregar.addEventListener("click",agregarTarea );
+
+
+})
